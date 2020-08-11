f9b995ab6470a88607ab1ad8343f79b5aa8fe23e
Mohammad Jafar Mashhadi <raindigital2007@gmail.com>
2020-08-10 22:12:39 -0600

    Moved model creation code out of the notebook

--- model_helper.py
+++ model_helper.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.python.ops import math_ops, array_ops
+import numpy
 
 
 class MaskStealingLayer(tf.keras.layers.Masking):
@@ -15,3 +16,35 @@ class MaskStealingLayer(tf.keras.layers.Masking):
         # Compute the mask and outputs simultaneously.
         outputs._keras_mask = array_ops.squeeze(boolean_mask, axis=-1)  # pylint: disable=protected-access
         return outputs
+
+
+def make_model(inputs, outputs, input_length, n_states, convs, grus):
+    n_in = len(inputs)
+    n_out = len(outputs)
+    n_features = n_in + n_out
+
+    bias_initializer = tf.keras.initializers.Constant(numpy.log(0.01))
+
+    signals = tf.keras.Input(shape=[input_length, n_features], name='signals')
+    mask = tf.keras.Input(shape=[input_length, 1], name='mask')
+
+    x = signals
+    x = MaskStealingLayer(0)((x, mask))
+
+    for filters, conv_size in convs:
+        x = tf.keras.layers.Conv1D(filters=filters, kernel_size=conv_size, padding="same", name=f'conv_{conv_size}')(x)
+
+    for gru_size in grus:
+        x = tf.keras.layers.GRU(gru_size, return_sequences=True)(x)
+        # x = tf.keras.layers.LeakyReLU()(x)
+
+    x = tf.keras.layers.Dense(grus[-1])(x)
+    x = tf.keras.layers.LeakyReLU()(x)
+
+    x = tf.keras.layers.Dense(n_states, bias_initializer=bias_initializer, activation='softmax')(x)
+
+    # x = tf.keras.layers.UpSampling1D(2 ** 1)(x)
+
+    model = tf.keras.Model(inputs=[signals, mask], outputs=x)
+
+    return model
